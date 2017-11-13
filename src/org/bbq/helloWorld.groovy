09b0812417c0488f45620819c84bab27ef172c75
bartja <bart.janssens2@cegeka.com>
2017-11-13 14:09:04 +0100

    Hello
    Signed-off-by: bartja <bart.janssens2@cegeka.com>

---
+++ src/org/bbq/helloWorld.groovy
@@ -0,0 +1,16 @@
+// HelloWorld
+package org.bbq;
+
+class hello implements Serializable {
+
+  private String name
+
+  def getName() {
+    name
+  }
+
+  def sayHello() {
+    echo "Hello, ${name}!"
+  }
+
+} // class hello
