260076cbc747672a8bc52337445ea45e829feab8
bartja <bart.janssens2@cegeka.com>
2017-11-14 09:25:13 +0100

    Escalate
    Signed-off-by: bartja <bart.janssens2@cegeka.com>

---
+++ src/org/bbq/Escalate.groovy
@@ -0,0 +1,28 @@
+// org.bbq.Escalate.groovy
+import java.io.File // For writing to a log file
+
+class Escalate implements Serializable {
+
+  def steps
+  def format = plain
+  def logfile = '/var/log/escalate'
+
+  // Constructor
+  Escalate( String target, format, logfile ) {
+    this.target = target
+    this.format = format
+    this.logfile = logfile
+  }
+
+  def escalate( String message ){
+    if( this.target == 'logfile' ) {
+      writelogfile( message )
+    }
+
+  def writelogfile( String message ){
+    new File(this.logfile).withWriter('utf-8') {
+      writer => writer.writeLine message
+    }
+  }
+
+} // class Escalate
