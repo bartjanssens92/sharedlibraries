c054650b2ef10c5eb5b323c2a2f544ad16ef2859
bartja <bart.janssens2@cegeka.com>
2017-11-29 12:31:47 +0100

    Create nested audit call
    Signed-off-by: bartja <bart.janssens2@cegeka.com>

--- src/org/bbq/helloWorld.groovy
+++ src/org/bbq/helloWorld.groovy
@@ -6,8 +6,22 @@ import org.bbq.Audit
 
 class helloWorld implements Serializable {
 
-  static def say( message ) {
-    script.sh( "echo $message" )
+  def steps
+  def env
+  def currentbuild
+
+  /**
+   * Testing class for passing the env and currentbuild envs
+   */
+  HelloWorld( Steps ){
+    this.steps = Steps
+    this.mAudit = new Audit( this.steps )
+    this.mAudit.env = env
+    this.mAudit.currentbuild = currentbuild
+  }
+
+  def call( String message ){
+    mAudit('nested audit message')
   }
 
 } // class hello
